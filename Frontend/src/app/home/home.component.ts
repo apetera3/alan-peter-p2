fd24e6835510ae116c8cf7d215d2fc4ec86ca3ee
Alan Peter <petera3@gmatc.matc.edu>
2023-10-09 07:53:04 +0300

    project complete blog

---
+++ Frontend/src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  //posts: Blog[];
+  posts: any[];
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit() {
+    this.postsService.getAllPosts().subscribe(
+      post_data=>{
+        this.posts = post_data.reverse();
+        console.log(this.posts);
+      }, error => {
+        console.log('error');
+      }
+    )
+    // this.http.get('data/posts.json').subscribe(
+    //   post_data=>{
+    //     this.posts = post_data;
+    //   }
+    // );
+  }
+
+}
